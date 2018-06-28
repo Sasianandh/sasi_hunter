ad029fa02b9ab51b1d0e4519bad41c3d708dda60
Sasianandh <31653406+Sasianandh@users.noreply.github.com>
2018-06-28 10:27:46 +0530

    Create pro1.c

---
+++ pro1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+char a[100];
+int i,j,k,m,n,x=0,y;
+gets(s);
+gets(a);
+m=strlen(s);
+n=strlen(a);
+for(i=0;i<m;i++)
+{
+    for(j=0;j<n;j++)
+    {
+        if(s[i]==a[i])
+        {
+            x++;
+            break;
+        }
+        else
+        break;
+    }
+    if(x>0)
+    {
+        y++;
+       x=0;
+    }
+    }
+    for(i=0;i<y;i++)
+{
+    printf("%c",s[i]);
+}
+}
+    
